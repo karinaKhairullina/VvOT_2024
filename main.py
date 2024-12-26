9ddff9ee7262689fe918935eb2058f7238a40376
karinaKhairullina <95102785+karinaKhairullina@users.noreply.github.com>
2024-12-26 22:46:49 +0300

    + gpt

--- main.py
+++ main.py
@@ -1,168 +1,111 @@
 import os
 import json
 import requests
-import boto3
+import base64
+from urls import *
 
-FUNC_RESPONSE = {'statusCode': 200, 'body': ''}
+# Отправка POST запросов
+def send_post(url, headers=None, json_data=None):
+    response = requests.post(url, headers=headers, json=json_data)
+    if response.status_code == 200:
+        return response.json()
+    return None
 
-TG_API_KEY = os.environ.get("TG_API_KEY")
-YANDX_GPT_API_KEY = os.environ.get("API_KEY")
-BUCKET_NAME = os.environ.get("BUCKET_NAME")
-OBJECT_KEY = os.environ.get("BUCKET_OBJECT")
-ACCESS_KEY = os.environ.get("ACCESS_KEY")
-SECRET_KEY = os.environ.get("SECRET_KEY")
-TELEGRAM_API_URL = f"https://api.telegram.org/bot{TG_API_KEY}"
+# Получение объекта из бакета
+def get_object_from_bucket(object_key):
+    with open(os.path.join(MOUNT_POINT, object_key), "r") as file:
+        return file.read()
 
-
-def send_message(chat_id, text):
-    try:
-        payload = {'chat_id': chat_id, 'text': text}
-        response = requests.post(url=f'{TELEGRAM_API_URL}/sendMessage', json=payload)
-        if response.status_code != 200:
-            return None
-    except Exception as e:
+# Обработка текста через гпт
+def get_answer_from_gpt(question):
+    instruction = get_object_from_bucket(BUCKET_OBJECT_KEY)
+    if not instruction:
         return None
 
-def get_instruction_from_storage():
-    s3_client = boto3.client(
-        "s3",
-        endpoint_url="https://storage.yandexcloud.net",
-        aws_access_key_id=ACCESS_KEY,
-        aws_secret_access_key=SECRET_KEY,
-    )
-    try:
-        response = s3_client.get_object(Bucket=BUCKET_NAME, Key=OBJECT_KEY)
-        instruction = response['Body'].read().decode('utf-8')
-        return instruction
-    except Exception as e:
-        return None
+    data = {
+        "modelUri": f"gpt://{FOLDER_ID}/yandexgpt",
+        "messages": [
+            {"role": "system", "text": instruction},
+            {"role": "user", "text": question},
+        ],
+    }
+    response = send_post(YC_API_GPT_URL, headers={"Authorization": f"Bearer {IAM_TOKEN}"}, json_data=data)
+    if response:
+        alternatives = response["result"]["alternatives"]
+        final_alternatives = [alt for alt in alternatives if alt["status"] == "ALTERNATIVE_STATUS_FINAL"]
+        return final_alternatives[0]["message"].get("text") if final_alternatives else None
+    return None
 
-# Функция для запроса к YandexGPT API
-def get_answer_from_yandex_gpt(prompt, instruction):
-    url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+# Распознавание текста с фото
+def recognize_text(base64_image):
+    data = {
+        "content": base64_image,
+        "mimeType": "image/jpeg",
+        "languageCodes": ["ru", "en"],
+    }
     headers = {
-        "Authorization": f"Api-Key {YANDX_GPT_API_KEY}",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {IAM_TOKEN}",
+        "x-folder-id": FOLDER_ID,
     }
-    payload = {
-        "prompt": f"{instruction}\nВопрос: {prompt}",
-        "temperature": 0.7,
-        "max_tokens": 1000
-    }
+    response = send_post(YC_API_OCR_URL, headers=headers, json_data=data)
+    if response:
+        text = response.get("result", {}).get("textAnnotation", {}).get("fullText", "")
+        return text.replace("-\n", "").replace("\n", " ") if text else None
+    return None
 
-    try:
-        response = requests.post(url, headers=headers, json=payload)
-        if response.status_code == 200:
-            return response.json().get("text", "").strip()
-        else:
-            return None
-    except Exception as e:
-        return None
+# Получения пути файла
+def get_file_path(file_id):
+    file_info = send_post(f"{TELEGRAM_API_URL}/getFile?file_id={file_id}")
+    return file_info["result"]["file_path"] if file_info else None
 
+# Получения изображения по пути
+def get_image(file_path):
+    file_url = f"{TELEGRAM_FILE_URL}/{file_path}"
+    return requests.get(file_url).content
 
-def process_image_with_yandex_ocr(image_url):
-    ocr_url = "https://vision.api.cloud.yandex.net/vision/v1/batchAnalyze"
-    headers = {
-        "Authorization": f"Api-Key {YANDX_GPT_API_KEY}",
-        "Content-Type": "application/json"
-    }
-    payload = {
-        "analyze_specs": [{
-            "content": image_url,
-            "features": [{"type": "TEXT_DETECTION"}]
-        }]
-    }
-    try:
-        response = requests.post(ocr_url, headers=headers, json=payload)
-        if response.status_code == 200:
-            ocr_result = response.json()
-            text = " ".join([annotation['description'] for annotation in ocr_result['results'][0]['textAnnotations']])
-            return text
-        else:
-            return None
-    except Exception as e:
-        return None
-
+# Обработчик
 def process_update(update):
 
-    message = update['message']
-    chat_id = message['chat']['id']
+    message = update["message"]
+    chat_id = message["chat"]["id"]
 
-    # Обработка текстового сообщения
-    if 'text' in message:
-        text = message['text'].strip()
-
+    if "text" in message:
+        text = message["text"]
         if text == "/start" or text == "/help":
-            response_text = (
-                "Я помогу подготовить ответ на экзаменационный вопрос по дисциплине 'Операционные системы'. "
-                "Пришлите мне фотографию с вопросом или наберите его текстом."
-            )
-            send_message(chat_id, response_text)
-            return
+            return send_message(chat_id,
+                                "Я помогу подготовить ответ на экзаменационный вопрос по дисциплине 'Операционные системы'.\nПришлите мне фотографию с вопросом или наберите его текстом.")
+        answer = get_answer_from_gpt(text)
+        return send_message(chat_id, answer if answer else "Я не смог подготовить ответ на экзаменационный вопрос.")
 
-        if text == "":
-            send_message(chat_id, "Пожалуйста, отправьте текст вопроса.")
-            return
+    if "photo" in message:
+        file_id = message["photo"][-1]["file_id"]
+        file_path = get_file_path(file_id)
 
-        instruction = get_instruction_from_storage()
-        if instruction is None:
-            send_message(chat_id, "Не удалось получить инструкцию для обработки.")
-            return
+        image = get_image(file_path)
+        base64_image = base64.b64encode(image).decode("utf-8")
+        recognized_text = recognize_text(base64_image)
+        if recognized_text:
+            answer = get_answer_from_gpt(recognized_text)
+            return send_message(chat_id, answer if answer else "Я не смог подготовить ответ на экзаменационный вопрос.")
+        return send_message(chat_id, "Я не могу обработать эту фотографию.")
 
-        response_text = get_answer_from_yandex_gpt(text, instruction)
-        if response_text:
-            send_message(chat_id, response_text)
-        else:
-            send_message(chat_id, "Я не смог подготовить ответ на экзаменационный вопрос.")
+    return send_message(chat_id, "Я могу обработать только текстовое сообщение или фотографию.")
 
-    # Обработка фотографии
-    elif 'photo' in message:
-        if len(message['photo']) > 1:
-            send_message(chat_id, "Я могу обработать только одну фотографию.")
-            return
 
-        # Получаем URL фотографии
-        file_id = message['photo'][-1]['file_id']
-        file_url = f"https://api.telegram.org/bot{TG_API_KEY}/getFile?file_id={file_id}"
-        response = requests.get(file_url)
-        file_path = response.json().get('result', {}).get('file_path')
-        if not file_path:
-            send_message(chat_id, "Не удалось получить фотографию.")
-            return
+# Отправка сообщения
+def send_message(chat_id, text):
+    send_post(f"{TELEGRAM_API_URL}/sendMessage", json_data={"chat_id": chat_id, "text": text})
 
-        image_url = f"https://api.telegram.org/file/bot{TG_API_KEY}/{file_path}"
 
-        # Обработка OCR на фотографии
-        text_from_image = process_image_with_yandex_ocr(image_url)
-        if text_from_image:
-            instruction = get_instruction_from_storage()
-            if instruction is None:
-                send_message(chat_id, "Не удалось получить инструкцию для обработки.")
-
-            response_text = get_answer_from_yandex_gpt(text_from_image, instruction)
-            if response_text:
-                send_message(chat_id, response_text)
-            else:
-                send_message(chat_id, "Я не смог подготовить ответ на экзаменационный вопрос.")
-        else:
-            send_message(chat_id, "Я не могу обработать эту фотографию.")
-
-    # Обработка других типов сообщений
-    else:
-        send_message(chat_id, "Я могу обработать только текстовое сообщение или фотографию.")
-
-# Основная функция для работы с Webhook
 def handler(event, context):
-    if TG_API_KEY is None:
+    if not TG_API_KEY:
         return FUNC_RESPONSE
 
     try:
-        update = json.loads(event['body'])
-    except (KeyError, json.JSONDecodeError) as e:
+        update = json.loads(event["body"])
+    except (KeyError, json.JSONDecodeError):
         return FUNC_RESPONSE
 
-    # Обработка обновления от Telegram
-    process_result = process_update(update)
-    print(process_result)
-
+    process_update(update)
     return FUNC_RESPONSE
